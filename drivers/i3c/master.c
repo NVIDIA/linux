c257a89317ae99c09dec3fbe7fdc74276e918cc5
Zbigniew Lukwinski <zbigniew.lukwinski@linux.intel.com>
2022-05-10 17:37:55 +0200

    i3c: master: Enable support for PEC
    PEC (Packet Error Check) support enabled in I3C controller driver. API
    for I3C device driver to control (enable or disable) PEC support in
    lower layer driver added.
    
    Signed-off-by: Zbigniew Lukwinski <zbigniew.lukwinski@linux.intel.com>
    Change-Id: I482431f9ab39947e3122a8afb7cbaa793233c594

--- drivers/i3c/master.c
+++ drivers/i3c/master.c
@@ -3572,6 +3572,25 @@ void i3c_dev_free_ibi_locked(struct i3c_dev_desc *dev)
 	dev->ibi = NULL;
 }
 
+int i3c_dev_control_pec(struct i3c_dev_desc *dev, bool pec)
+{
+	struct i3c_master_controller *master = i3c_dev_get_master(dev);
+
+	if (!master->pec_supported)
+		return -EOPNOTSUPP;
+
+	dev->info.pec = pec;
+
+	/*
+	 * TODO: There are two cases which shall be covered
+	 * 1. Controller doesn't support PEC.
+	 *    In this case we could just fallback to SW implementation.
+	 * 2. Device doesn't support PEC.
+	 *    Then we really can't use PEC - and should error-out.
+	 */
+	return 0;
+}
+
 static int __init i3c_init(void)
 {
 	int res;
