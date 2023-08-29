c14d4b6793771cfd3742d66b449076375b12d3af
Sumanth Bhat <sumanth.bhat@intel.com>
2023-08-28 22:57:52 -0700

    i3c-debug: Introduce MIPI Debug for I3C extension CCCs
    MIPI Debug for I3C Specification v1.0 introduces an extension set to
    the existing CCC list defined by the MIPI I3C specification. This patch
    adds the same to the existing CCC set.
    
    Signed-off-by: Sumanth Bhat <sumanth.bhat@intel.com>
    Change-Id: Idd0978dfad3a62c2701cb7260eb14165b59ef46c

--- drivers/i3c/internals.h
+++ drivers/i3c/internals.h
@@ -27,6 +27,22 @@ int i3c_dev_enable_ibi_locked(struct i3c_dev_desc *dev);
 int i3c_dev_request_ibi_locked(struct i3c_dev_desc *dev,
 			       const struct i3c_ibi_setup *req);
 void i3c_dev_free_ibi_locked(struct i3c_dev_desc *dev);
+int i3c_dev_getstatus_locked(struct i3c_dev_desc *dev, struct i3c_device_info *info);
+int i3c_master_getmrl_locked(struct i3c_master_controller *master, struct i3c_device_info *info);
+int i3c_master_getmwl_locked(struct i3c_master_controller *master, struct i3c_device_info *info);
+int i3c_master_setmrl_locked(struct i3c_master_controller *master,
+			     struct i3c_device_info *info, __be16 read_len, u8 ibi_len);
+int i3c_master_setmwl_locked(struct i3c_master_controller *master,
+			     struct i3c_device_info *info, __be16 write_len);
+
+int i3c_dev_dbgaction_wr_locked(struct i3c_dev_desc *dev, struct i3c_device_info *info,
+				u8 *data, u8 len);
+int i3c_dev_dbgopcode_wr_locked(struct i3c_dev_desc *dev, struct i3c_device_info *info,
+				u8 *data, u8 len);
+int i3c_dev_dbgopcode_rd_locked(struct i3c_dev_desc *dev, struct i3c_device_info *info,
+				u8 *data, u8 len);
+
+int i3c_for_each_dev(void *data, int (*fn)(struct device *, void *));
 int i3c_dev_generate_ibi_locked(struct i3c_dev_desc *dev, const u8 *data, int len);
 int i3c_dev_pending_read_notify_locked(struct i3c_dev_desc *dev,
 				       struct i3c_priv_xfer *pending_read,
