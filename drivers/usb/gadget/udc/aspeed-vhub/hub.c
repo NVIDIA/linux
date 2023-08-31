3d811bf36b63bf63398b46e876b3dcc1eb59fa14
Joe Wang <joe_wang@aspeedtech.com>
2023-08-31 11:13:06 +0800

    usb: aspeed_vhub: align vhub driver with kernel 5.15
    Align vhub driver with kernel 5.15 for the following items:
    1. Enable AUTO_REMOTE_WAKEUP by default
    2. Change to 'else if' condition for dev feature selectors
    3. Save and response the current configuration value
    
    Signed-off-by: Joe Wang <joe_wang@aspeedtech.com>
    Change-Id: Ie01fbc71a730b0909e57ebfe890b269011c597ae
    (cherry picked from commit c3631ce31c47c6a9f3b8cc6f4f196d9187fd3fde)

--- drivers/usb/gadget/udc/aspeed-vhub/hub.c
+++ drivers/usb/gadget/udc/aspeed-vhub/hub.c
@@ -222,9 +222,8 @@ static int ast_vhub_hub_dev_feature(struct ast_vhub_ep *ep,
 		EPDBG(ep, "Hub remote wakeup %s\n",
 		      str_enabled_disabled(is_set));
 		return std_req_complete;
-	}
 
-	if (wValue == USB_DEVICE_TEST_MODE) {
+	} else if (wValue == USB_DEVICE_TEST_MODE) {
 		val = readl(ep->vhub->regs + AST_VHUB_CTRL);
 		val &= ~GENMASK(10, 8);
 		val |= VHUB_CTRL_SET_TEST_MODE((wIndex >> 8) & 0x7);
@@ -446,10 +445,9 @@ enum std_req_rc ast_vhub_std_hub_request(struct ast_vhub_ep *ep,
 
 		/* GET/SET_CONFIGURATION */
 	case DeviceRequest | USB_REQ_GET_CONFIGURATION:
-		return ast_vhub_simple_reply(ep, 1);
+		return ast_vhub_simple_reply(ep, vhub->current_config);
 	case DeviceOutRequest | USB_REQ_SET_CONFIGURATION:
-		if (wValue != 1)
-			return std_req_stall;
+		vhub->current_config = wValue;
 		return std_req_complete;
 
 		/* GET_DESCRIPTOR */
