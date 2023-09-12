31681f4bda52b67c586807483b766e05db7f9949
Dylan Hung <dylan_hung@aspeedtech.com>
2023-09-12 14:21:23 +0800

    i3c: dw: Get reset_control struct without reset-name
    Use "NULL" instead of "core_rst" to get the reser_control structure as
    there is no definition for the reset-control and reset-name.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I7c52e05148ff4257436338aa0b8950f516b2c8ab

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -2182,7 +2182,7 @@ int dw_i3c_common_probe(struct dw_i3c_master *master,
 		return PTR_ERR(master->pclk);
 
 	master->core_rst = devm_reset_control_get_optional_exclusive(&pdev->dev,
-								    "core_rst");
+								    NULL);
 	if (IS_ERR(master->core_rst))
 		return PTR_ERR(master->core_rst);
 
