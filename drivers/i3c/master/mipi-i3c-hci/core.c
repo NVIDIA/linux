f1ff733c24f2af3d0e5de912ffd0b7593ec1b37b
Billy Tsai <billy_tsai@aspeedtech.com>
2023-11-03 19:14:03 +0800

    i3c: mipi-i3c-hci: Return I3C_ERROR_M2 when ERR_ADDR_HEADER
    When an I3C xfer error is due to an address header error, it is expected.
    Therefore, the driver should return the I3C_ERROR_M2 code to signal the
    subsystem to ignore it.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I48fc23538494705ea039c1d30a55c981e87087cb

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -459,6 +459,7 @@ static int i3c_hci_send_ccc_cmd(struct i3c_master_controller *m,
 			ccc->err = I3C_ERROR_M2;
 			fallthrough;
 		default:
+			DBG("resp status = %lx", RESP_STATUS(xfer[i].response));
 			ret = -EIO;
 			goto out;
 		}
