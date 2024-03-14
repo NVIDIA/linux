8585decb2ed2fbf629a8a7a4079ee7e24366897f
Radivoje (Ogi) Jovanovic <rjovanovic@nvidia.com>
2024-03-14 11:33:17 -0700

    add w25q512jvbim in the kernel
    Signed-off-by: Radivoje (Ogi) Jovanovic <rjovanovic@nvidia.com>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -298,6 +298,11 @@ static const struct flash_info winbond_nor_parts[] = {
 		.id = SNOR_ID(0xef, 0x70, 0x21),
 		.fixups = &winbond_nor_multi_die_fixups,
 	}, {
+		.id = SNOR_ID(0xef, 0x70, 0x20),
+		.name = "w25q512jvbim",
+		.size = SZ_64M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
 		.id = SNOR_ID(0xef, 0x50, 0x12),
 		.name = "w25q20bw",
 		.size = SZ_256K,
