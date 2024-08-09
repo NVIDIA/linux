b638801d014da972e57cc4fe3297cf65e547c8c8
Billy Tsai <billy_tsai@aspeedtech.com>
2024-08-09 17:34:25 +0800

    i3c/master: hci: dma: Remove the A0-related code
    The DMA mode can’t execute on the A0 chip, so there’s no need to check
    the chip version for specific behavior.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Icba99894e39ee1438cfab0cdcdbdcfc27d119f78

--- drivers/i3c/master/mipi-i3c-hci/dma.c
+++ drivers/i3c/master/mipi-i3c-hci/dma.c
@@ -566,40 +566,20 @@ static void hci_dma_xfer_done(struct i3c_hci *hci, struct hci_rh_data *rh)
 		tid = RESP_TID(resp);
 		dev_dbg(&hci->master.dev, "resp = 0x%08x", resp);
 		if (hci->master.target) {
-			if (!aspeed_get_i3c_revision_id(hci)) {
-				dev_dbg(&hci->master.dev, a0_debug_s,
-					TARGET_RESP_STATUS(resp),
-					TARGET_RESP_XFER_TYPE(resp),
-					TARGET_RESP_TID_A0(resp),
-					TARGET_RESP_CCC_HDR(resp),
-					TARGET_RESP_DATA_LENGTH(resp));
-				/* ibi or master read or HDR read */
-				if (!TARGET_RESP_CCC_HDR(resp) ||
-				    TARGET_RESP_CCC_HDR(resp) & 0x80) {
-					if (TARGET_RESP_TID_A0(resp) ==
-					    TID_TARGET_IBI)
-						complete(&hci->ibi_comp);
-					else if (TARGET_RESP_TID_A0(resp) ==
-						 TID_TARGET_RD_DATA)
-						complete(&hci->pending_r_comp);
-				}
-			} else {
-				dev_dbg(&hci->master.dev, a1_debug_s,
-					TARGET_RESP_STATUS(resp),
-					TARGET_RESP_XFER_TYPE(resp),
-					TARGET_RESP_CCC_INDICATE(resp),
-					TARGET_RESP_TID(resp),
-					TARGET_RESP_CCC_HDR(resp),
-					TARGET_RESP_DATA_LENGTH(resp));
-				/* ibi or master read or HDR read */
-				if (!TARGET_RESP_CCC_INDICATE(resp)) {
-					if (TARGET_RESP_TID(resp) ==
-					    TID_TARGET_IBI)
-						complete(&hci->ibi_comp);
-					else if (TARGET_RESP_TID(resp) ==
-						 TID_TARGET_RD_DATA)
-						complete(&hci->pending_r_comp);
-				}
+			dev_dbg(&hci->master.dev, a1_debug_s,
+				TARGET_RESP_STATUS(resp),
+				TARGET_RESP_XFER_TYPE(resp),
+				TARGET_RESP_CCC_INDICATE(resp),
+				TARGET_RESP_TID(resp),
+				TARGET_RESP_CCC_HDR(resp),
+				TARGET_RESP_DATA_LENGTH(resp));
+			/* ibi or master read or HDR read */
+			if (!TARGET_RESP_CCC_INDICATE(resp)) {
+				if (TARGET_RESP_TID(resp) == TID_TARGET_IBI)
+					complete(&hci->ibi_comp);
+				else if (TARGET_RESP_TID(resp) ==
+					 TID_TARGET_RD_DATA)
+					complete(&hci->pending_r_comp);
 			}
 		}
 		xfer = rh->src_xfers[done_ptr];
@@ -716,21 +696,13 @@ static void hci_dma_process_ibi(struct i3c_hci *hci, struct hci_rh_data *rh)
 		if (hci->master.target) {
 			dev = hci->master.this;
 			size_t nbytes = TARGET_RESP_DATA_LENGTH(ibi_status);
-			if (!aspeed_get_i3c_revision_id(hci))
-				dev_dbg(&hci->master.dev, a0_debug_s,
-					TARGET_RESP_STATUS(ibi_status),
-					TARGET_RESP_XFER_TYPE(ibi_status),
-					TARGET_RESP_TID_A0(ibi_status),
-					TARGET_RESP_CCC_HDR(ibi_status),
-					TARGET_RESP_DATA_LENGTH(ibi_status));
-			else
-				dev_dbg(&hci->master.dev, a1_debug_s,
-					TARGET_RESP_STATUS(ibi_status),
-					TARGET_RESP_XFER_TYPE(ibi_status),
-					TARGET_RESP_CCC_INDICATE(ibi_status),
-					TARGET_RESP_TID(ibi_status),
-					TARGET_RESP_CCC_HDR(ibi_status),
-					TARGET_RESP_DATA_LENGTH(ibi_status));
+			dev_dbg(&hci->master.dev, a1_debug_s,
+				TARGET_RESP_STATUS(ibi_status),
+				TARGET_RESP_XFER_TYPE(ibi_status),
+				TARGET_RESP_CCC_INDICATE(ibi_status),
+				TARGET_RESP_TID(ibi_status),
+				TARGET_RESP_CCC_HDR(ibi_status),
+				TARGET_RESP_DATA_LENGTH(ibi_status));
 			if (TARGET_RESP_XFER_TYPE(ibi_status)) {
 				chunks = DIV_ROUND_UP(nbytes, rh->ibi_chunk_sz);
 				ibi_chunks += chunks;
@@ -866,23 +838,13 @@ static void hci_dma_process_ibi(struct i3c_hci *hci, struct hci_rh_data *rh)
 	}
 	if (hci->master.target) {
 		/* Bypass the priv_xfer data to target layer */
-		if (!aspeed_get_i3c_revision_id(hci)) {
-			if (dev->target_info.read_handler &&
-			    !TARGET_RESP_CCC_HDR(ibi_status))
-				dev->target_info.read_handler(dev->dev,
-							      hci->target_rx.buf,
-							      ibi_size);
-			if (TARGET_RESP_CCC_HDR(ibi_status))
-				aspeed_i3c_ccc_handler(hci, TARGET_RESP_CCC_HDR(ibi_status));
-		} else {
-			if (dev->target_info.read_handler &&
-			    !TARGET_RESP_CCC_INDICATE(ibi_status))
-				dev->target_info.read_handler(dev->dev,
-							      hci->target_rx.buf,
-							      ibi_size);
-			if (TARGET_RESP_CCC_INDICATE(ibi_status))
-				aspeed_i3c_ccc_handler(hci, TARGET_RESP_CCC_HDR(ibi_status));
-		}
+		if (dev->target_info.read_handler &&
+		    !TARGET_RESP_CCC_INDICATE(ibi_status))
+			dev->target_info.read_handler(dev->dev,
+						      hci->target_rx.buf,
+						      ibi_size);
+		if (TARGET_RESP_CCC_INDICATE(ibi_status))
+			aspeed_i3c_ccc_handler(hci, TARGET_RESP_CCC_HDR(ibi_status));
 	} else {
 		/* submit it */
 		slot->dev = dev;
