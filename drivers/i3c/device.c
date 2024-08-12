f7fe35ed5a17fc7ebf9c9168b7119a8f738b5a67
Billy Tsai <billy_tsai@aspeedtech.com>
2024-08-12 18:06:53 +0800

    i3c: device: Sync the info with device internal data.
    The SETMRL/GETMRL/SETMWL/GETMWL/GETSTATUS ccc will get or change the
    info about the device when using these api the info should be synchronized
    with the device internal data and not just filled into the temporary
    memory from the caller.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Ib20b62676bb1435616d498488547007a32a6fb16

--- drivers/i3c/device.c
+++ drivers/i3c/device.c
@@ -211,8 +211,9 @@ int i3c_device_getstatus_ccc(struct i3c_device *dev, struct i3c_device_info *inf
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (dev->desc)
-		ret = i3c_dev_getstatus_locked(dev->desc, info);
+		ret = i3c_dev_getstatus_locked(dev->desc, &dev->desc->info);
 	i3c_bus_normaluse_unlock(dev->bus);
+	i3c_device_get_info(dev, info);
 
 	return ret;
 }
@@ -478,8 +479,9 @@ int i3c_device_setmrl_ccc(struct i3c_device *dev, struct i3c_device_info *info, 
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (master)
-		ret = i3c_master_setmrl_locked(master, info, read_len, ibi_len);
+		ret = i3c_master_setmrl_locked(master, &dev->desc->info, read_len, ibi_len);
 	i3c_bus_normaluse_unlock(dev->bus);
+	i3c_device_get_info(dev, info);
 
 	return ret;
 }
@@ -503,8 +505,9 @@ int i3c_device_setmwl_ccc(struct i3c_device *dev, struct i3c_device_info *info, 
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (master)
-		ret = i3c_master_setmwl_locked(master, info, write_len);
+		ret = i3c_master_setmwl_locked(master, &dev->desc->info, write_len);
 	i3c_bus_normaluse_unlock(dev->bus);
+	i3c_device_get_info(dev, info);
 
 	return ret;
 }
@@ -527,8 +530,9 @@ int i3c_device_getmrl_ccc(struct i3c_device *dev, struct i3c_device_info *info)
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (master)
-		ret = i3c_master_getmrl_locked(master, info);
+		ret = i3c_master_getmrl_locked(master, &dev->desc->info);
 	i3c_bus_normaluse_unlock(dev->bus);
+	i3c_device_get_info(dev, info);
 
 	return ret;
 }
@@ -551,8 +555,9 @@ int i3c_device_getmwl_ccc(struct i3c_device *dev, struct i3c_device_info *info)
 
 	i3c_bus_normaluse_lock(dev->bus);
 	if (master)
-		ret = i3c_master_getmwl_locked(master, info);
+		ret = i3c_master_getmwl_locked(master, &dev->desc->info);
 	i3c_bus_normaluse_unlock(dev->bus);
+	i3c_device_get_info(dev, info);
 
 	return ret;
 }
