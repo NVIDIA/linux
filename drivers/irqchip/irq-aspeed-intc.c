111e21d30aefd9e7937b62a0197a69b6a78eb6b9
Kevin Chen <kevin_chen@aspeedtech.com>
2024-09-20 16:14:45 +0800

    drivers: irqchip: Refine to the same init function
    1. Use the same init funciton in INTC0 or INTC1.
    2. Use scoped_guard instead of raw_spin_lock_irqsave in ISR.
    
    Change-Id: I5e1359e7bb420390f4eb9f5cebdb3a60e10c5d41
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- drivers/irqchip/irq-aspeed-intc.c
+++ drivers/irqchip/irq-aspeed-intc.c
@@ -17,6 +17,7 @@
 
 #define INTC_INT_ENABLE_REG	0x00
 #define INTC_INT_STATUS_REG	0x04
+#define INTC_IRQS_PER_WORD	32
 
 struct aspeed_intc_ic {
 	void __iomem		*base;
@@ -29,17 +30,18 @@ static void aspeed_intc_ic_irq_handler(struct irq_desc *desc)
 {
 	struct aspeed_intc_ic *intc_ic = irq_desc_get_handler_data(desc);
 	struct irq_chip *chip = irq_desc_get_chip(desc);
-	unsigned long bit, status, flags;
 
 	chained_irq_enter(chip, desc);
 
-	raw_spin_lock_irqsave(&intc_ic->gic_lock, flags);
-	status = readl(intc_ic->base + INTC_INT_STATUS_REG);
-	for_each_set_bit(bit, &status, 32) {
-		generic_handle_domain_irq(intc_ic->irq_domain, bit);
-		writel(BIT(bit), intc_ic->base + INTC_INT_STATUS_REG);
+	scoped_guard(raw_spinlock, &intc_ic->gic_lock) {
+		unsigned long bit, status;
+
+		status = readl(intc_ic->base + INTC_INT_STATUS_REG);
+		for_each_set_bit(bit, &status, INTC_IRQS_PER_WORD) {
+			generic_handle_domain_irq(intc_ic->irq_domain, bit);
+			writel(BIT(bit), intc_ic->base + INTC_INT_STATUS_REG);
+		}
 	}
-	raw_spin_unlock_irqrestore(&intc_ic->gic_lock, flags);
 
 	chained_irq_exit(chip, desc);
 }
@@ -48,40 +50,28 @@ static void aspeed_intc_irq_mask(struct irq_data *data)
 {
 	struct aspeed_intc_ic *intc_ic = irq_data_get_irq_chip_data(data);
 	unsigned int mask = readl(intc_ic->base + INTC_INT_ENABLE_REG) & ~BIT(data->hwirq);
-	unsigned long flags;
 
-	raw_spin_lock_irqsave(&intc_ic->intc_lock, flags);
+	guard(raw_spinlock)(&intc_ic->intc_lock);
 	writel(mask, intc_ic->base + INTC_INT_ENABLE_REG);
-	raw_spin_unlock_irqrestore(&intc_ic->intc_lock, flags);
 }
 
 static void aspeed_intc_irq_unmask(struct irq_data *data)
 {
 	struct aspeed_intc_ic *intc_ic = irq_data_get_irq_chip_data(data);
 	unsigned int unmask = readl(intc_ic->base + INTC_INT_ENABLE_REG) | BIT(data->hwirq);
-	unsigned long flags;
 
-	raw_spin_lock_irqsave(&intc_ic->intc_lock, flags);
+	guard(raw_spinlock)(&intc_ic->intc_lock);
 	writel(unmask, intc_ic->base + INTC_INT_ENABLE_REG);
-	raw_spin_unlock_irqrestore(&intc_ic->intc_lock, flags);
-}
-
-static int aspeed_intc_irq_set_affinity(struct irq_data *data,
-					  const struct cpumask *dest,
-					  bool force)
-{
-	return -EINVAL;
 }
 
 static struct irq_chip aspeed_intc_chip = {
 	.name			= "ASPEED INTC",
 	.irq_mask		= aspeed_intc_irq_mask,
 	.irq_unmask		= aspeed_intc_irq_unmask,
-	.irq_set_affinity	= aspeed_intc_irq_set_affinity,
 };
 
-static int aspeed_intc_ic_map_irq_domain(struct irq_domain *domain,
-					unsigned int irq, irq_hw_number_t hwirq)
+static int aspeed_intc_ic_map_irq_domain(struct irq_domain *domain, unsigned int irq,
+					 irq_hw_number_t hwirq)
 {
 	irq_set_chip_and_handler(irq, &aspeed_intc_chip, handle_level_irq);
 	irq_set_chip_data(irq, domain->host_data);
@@ -94,59 +84,7 @@ static const struct irq_domain_ops aspeed_intc_ic_irq_domain_ops = {
 };
 
 static int __init aspeed_intc_ic_of_init(struct device_node *node,
-					struct device_node *parent)
-{
-	struct aspeed_intc_ic *intc_ic;
-	int ret = 0;
-	int irq;
-
-	intc_ic = kzalloc(sizeof(*intc_ic), GFP_KERNEL);
-	if (!intc_ic)
-		return -ENOMEM;
-
-	intc_ic->base = of_iomap(node, 0);
-	if (!intc_ic->base) {
-		pr_err("Failed to iomap intc_ic base\n");
-		ret = -ENOMEM;
-		goto err_free_ic;
-	}
-	writel(0xffffffff, intc_ic->base + INTC_INT_STATUS_REG);
-	writel(0x0, intc_ic->base + INTC_INT_ENABLE_REG);
-
-	irq = irq_of_parse_and_map(node, 0);
-	if (!irq) {
-		pr_err("Failed to get irq number\n");
-		ret = -EINVAL;
-		goto err_iounmap;
-	}
-
-	intc_ic->irq_domain = irq_domain_add_linear(node, 32,
-						   &aspeed_intc_ic_irq_domain_ops,
-						   intc_ic);
-	if (!intc_ic->irq_domain) {
-		ret = -ENOMEM;
-		goto err_iounmap;
-	}
-
-	raw_spin_lock_init(&intc_ic->gic_lock);
-	raw_spin_lock_init(&intc_ic->intc_lock);
-
-	intc_ic->irq_domain->name = "aspeed-intc-domain";
-
-	irq_set_chained_handler_and_data(irq,
-					 aspeed_intc_ic_irq_handler, intc_ic);
-
-	return 0;
-
-err_iounmap:
-	iounmap(intc_ic->base);
-err_free_ic:
-	kfree(intc_ic);
-	return ret;
-}
-
-static int __init aspeed_intc_ic_of_init_v2(struct device_node *node,
-					    struct device_node *parent)
+					 struct device_node *parent)
 {
 	struct aspeed_intc_ic *intc_ic;
 	int ret = 0;
@@ -165,9 +103,8 @@ static int __init aspeed_intc_ic_of_init_v2(struct device_node *node,
 	writel(0xffffffff, intc_ic->base + INTC_INT_STATUS_REG);
 	writel(0x0, intc_ic->base + INTC_INT_ENABLE_REG);
 
-	intc_ic->irq_domain = irq_domain_add_linear(node, 32,
-						    &aspeed_intc_ic_irq_domain_ops,
-						    intc_ic);
+	intc_ic->irq_domain = irq_domain_add_linear(node, INTC_IRQS_PER_WORD,
+						    &aspeed_intc_ic_irq_domain_ops, intc_ic);
 	if (!intc_ic->irq_domain) {
 		ret = -ENOMEM;
 		goto err_iounmap;
@@ -176,17 +113,19 @@ static int __init aspeed_intc_ic_of_init_v2(struct device_node *node,
 	raw_spin_lock_init(&intc_ic->gic_lock);
 	raw_spin_lock_init(&intc_ic->intc_lock);
 
-	intc_ic->irq_domain->name = "aspeed-intc-domain";
-
+	/* Check all the irq numbers valid. If not, unmaps all the base and frees the data. */
 	for (i = 0; i < of_irq_count(node); i++) {
 		irq = irq_of_parse_and_map(node, i);
 		if (!irq) {
 			pr_err("Failed to get irq number\n");
 			ret = -EINVAL;
 			goto err_iounmap;
-		} else {
-			irq_set_chained_handler_and_data(irq, aspeed_intc_ic_irq_handler, intc_ic);
 		}
+	}
+
+	for (i = 0; i < of_irq_count(node); i++) {
+		irq = irq_of_parse_and_map(node, i);
+		irq_set_chained_handler_and_data(irq, aspeed_intc_ic_irq_handler, intc_ic);
 	}
 
 	return 0;
@@ -199,4 +138,3 @@ err_free_ic:
 }
 
 IRQCHIP_DECLARE(ast2700_intc_ic, "aspeed,ast2700-intc-ic", aspeed_intc_ic_of_init);
-IRQCHIP_DECLARE(ast2700_intc_icv2, "aspeed,ast2700-intc-icv2", aspeed_intc_ic_of_init_v2);
