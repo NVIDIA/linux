b325988cc1085af15c1cc5c85926e33e9dfe549d
Billy Tsai <billy_tsai@aspeedtech.com>
2024-10-14 13:32:28 +0800

    i3c: mipi-i3c-hci: aspeed: Fix the remove issue
    Use the i3c_unregister to replace the i3c_master_unregister for
    compatibility with the target mode.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Ia35036783353ea1d6941d95ea7957987d8ff52f2

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -1486,7 +1486,7 @@ static void i3c_hci_remove(struct platform_device *pdev)
 {
 	struct i3c_hci *hci = platform_get_drvdata(pdev);
 
-	i3c_master_unregister(&hci->master);
+	i3c_unregister(&hci->master);
 }
 
 static const __maybe_unused struct of_device_id i3c_hci_of_match[] = {
