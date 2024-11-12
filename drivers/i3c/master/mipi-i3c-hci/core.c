10bb0ce4d23bd59b50c298542d245aec805ddb8d
Billy Tsai <billy_tsai@aspeedtech.com>
2024-11-12 14:13:17 +0800

    i3c: mipi-i3c-hci: aspeed: Support SETDASA CCC
    Allocate the entry with static address if the device information doesn't
    have the dynamic address.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Icb8c0457df5d5eb34f33549f564d569dd31dafd7

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -708,7 +708,8 @@ static int i3c_hci_attach_i3c_dev(struct i3c_dev_desc *dev)
 		return -ENOMEM;
 	if (hci->cmd == &mipi_i3c_hci_cmd_v1) {
 #ifdef CONFIG_ARCH_ASPEED
-		ret = mipi_i3c_hci_dat_v1.alloc_entry(hci, dev->info.dyn_addr);
+		ret = mipi_i3c_hci_dat_v1.alloc_entry(hci,
+						      dev->info.dyn_addr ?: dev->info.static_addr);
 #else
 		ret = mipi_i3c_hci_dat_v1.alloc_entry(hci);
 #endif
