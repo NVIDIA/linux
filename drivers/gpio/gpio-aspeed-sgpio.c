9bf9dacadcde7aefddc9c7a90c2000a2f107b5d4
Billy Tsai <billy_tsai@aspeedtech.com>
2025-03-26 15:42:26 +0800

    gpio: aspeed-sgpio: SGPIOS pins 64-71 should use serial in/out
    SGPIO slave pins 64–71 are used to communicate with the SGPIO master.
    Therefore, input/output control should be handled through the serial
    registers instead of the parallel registers.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I2f0d6327da8a7741005e626d5c698eb154f99daa

--- drivers/gpio/gpio-aspeed-sgpio.c
+++ drivers/gpio/gpio-aspeed-sgpio.c
@@ -41,6 +41,9 @@
 #define SELECT_FROM_PARALLEL_IN 1
 #define SELECT_FROM_SERIAL_IN 1
 
+#define BMC_CONTROL_START_INDEX 128
+#define BMC_CONTROL_END_INDEX 143
+
 static inline u32 field_get(u32 _mask, u32 _val)
 {
 	return (((_val) & (_mask)) >> (ffs(_mask) - 1));
@@ -212,6 +215,14 @@ static bool aspeed_sgpio_is_input(unsigned int offset)
 	return !(offset % 2);
 }
 
+static bool aspeed_sgpios_ctrl_by_csr(unsigned int offset)
+{
+	if (offset >= BMC_CONTROL_START_INDEX &&
+	    offset <= BMC_CONTROL_END_INDEX)
+		return true;
+	return false;
+}
+
 static int aspeed_sgpio_get(struct gpio_chip *gc, unsigned int offset)
 {
 	struct aspeed_sgpio *gpio = gpiochip_get_data(gc);
@@ -224,7 +235,7 @@ static int aspeed_sgpio_get(struct gpio_chip *gc, unsigned int offset)
 	raw_spin_lock_irqsave(&gpio->lock, flags);
 
 	if (gpio->version == 7) {
-		if (gpio->pdata->slave)
+		if (gpio->pdata->slave && !aspeed_sgpios_ctrl_by_csr(offset))
 			reg = aspeed_sgpio_is_input(offset) ?
 				      SGPIO_G7_PARALLEL_IN_DATA :
 				      SGPIO_G7_PARALLEL_OUT_DATA;
@@ -279,7 +290,7 @@ static int sgpio_g7_set_value(struct gpio_chip *gc, unsigned int offset,
 	if (aspeed_sgpio_is_input(offset))
 		return -EINVAL;
 
-	if (gpio->pdata->slave) {
+	if (gpio->pdata->slave && !aspeed_sgpios_ctrl_by_csr(offset)) {
 		// Ensure the parallel out value control by the software.
 		ast_write_bits(addr, SGPIO_G7_PARALLEL_OUT_SEL,
 			       SELECT_FROM_CSR);
