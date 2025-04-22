2cff6f27995b436c9646da921611e3330b77a175
Billy Tsai <billy_tsai@aspeedtech.com>
2025-04-22 15:02:34 +0800

    gpio: aspeed-sgpio: Fix the irq issue
    Corrects the number of banks processed during interrupt handling for
    ASPEED G7 SGPIO.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I24576630e3afba3842cee6e8a39d180c9fe27385

--- drivers/gpio/gpio-aspeed-sgpio.c
+++ drivers/gpio/gpio-aspeed-sgpio.c
@@ -644,7 +644,7 @@ static void aspeed_g7_sgpio_irq_handler(struct irq_desc *desc)
 
 	chained_irq_enter(ic, desc);
 
-	banks = DIV_ROUND_UP(gpio->chip.ngpio >> 2, 32);
+	banks = DIV_ROUND_UP(gpio->chip.ngpio >> 1, 32);
 	for (i = 0; i < banks; i++) {
 		addr = gpio->base + SGPIO_G7_IRQ_STS_OFFSET(i);
 
