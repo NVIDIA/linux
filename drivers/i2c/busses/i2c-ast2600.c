be41699616a55b5e6efa784a9e4d0dde9895d76c
Tommy Huang <tommy_huang@aspeedtech.com>
2025-07-21 13:22:29 +0800

    drivers: i2c: consolidate SLAVE_PENDING, TX_NAK and STOP handling
    Combine the handling of SLAVE_PENDING, TX_NAK, and STOP into a single IRQ
    case. This change separates the original logic into two stages:
    
    1. The STOP stage is handled in the current IRQ.
    2. The next packet stage will be handled in the following IRQ.
    
    This refactoring improves clarity and reduces redundancy in the interrupt
    handling logic.
    
    * code[1] revised by Chester Lin to be compatible with NvBMC kernel
    code base.
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: Iecafffd1907c8c3a160d70fb3a38de2dbf90562e
    Link: https://github.com/AspeedTech-BMC/linux/commit/bd4e6bdcbf4b9480a452b6c5c90d1c572695b85a [1]
    Acked-by: Chester Lin <chesterl@nvidia.com>

--- drivers/i2c/busses/i2c-ast2600.c
+++ drivers/i2c/busses/i2c-ast2600.c
@@ -842,22 +842,6 @@ static void ast2600_i2c_slave_packet_buff_irq(struct ast2600_i2c_bus *i2c_bus, u
 		       i2c_bus->reg_base + AST2600_I2CC_BUFF_CTRL);
 		cmd = SLAVE_TRIGGER_CMD | AST2600_I2CS_TX_BUFF_EN;
 		break;
-	case AST2600_I2CS_SLAVE_PENDING | AST2600_I2CS_STOP |
-	     AST2600_I2CS_TX_NAK | AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_RX_DONE:
-	case AST2600_I2CS_SLAVE_PENDING | AST2600_I2CS_WAIT_RX_DMA | AST2600_I2CS_STOP |
-	     AST2600_I2CS_TX_NAK | AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_RX_DONE:
-		i2c_slave_event(i2c_bus->slave[idx], I2C_SLAVE_STOP, &value);
-		i2c_slave_event(i2c_bus->slave[idx], I2C_SLAVE_WRITE_REQUESTED, &value);
-		slave_rx_len = AST2600_I2CC_GET_RX_BUF_LEN(readl(i2c_bus->reg_base +
-						AST2600_I2CC_BUFF_CTRL));
-		for (i = 0; i < slave_rx_len; i++) {
-			value = readb(i2c_bus->buf_base + 0x10 + i);
-			i2c_slave_event(i2c_bus->slave[idx], I2C_SLAVE_WRITE_RECEIVED, &value);
-		}
-		writel(AST2600_I2CC_SET_RX_BUF_LEN(i2c_bus->buf_size),
-		       i2c_bus->reg_base + AST2600_I2CC_BUFF_CTRL);
-		cmd = SLAVE_TRIGGER_CMD | AST2600_I2CS_RX_BUFF_EN;
-		break;
 	case AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_WAIT_TX_DMA | AST2600_I2CS_RX_DONE:
 	case AST2600_I2CS_WAIT_TX_DMA | AST2600_I2CS_RX_DONE:
 	case AST2600_I2CS_WAIT_TX_DMA:
@@ -903,7 +887,12 @@ static void ast2600_i2c_slave_packet_buff_irq(struct ast2600_i2c_bus *i2c_bus, u
 		}
 #endif
 		break;
-
+	/* the pending slave needs to be cleared with TX_NAK and STOP here */
+	/* other flags will be handled in the next irq callback */
+	case AST2600_I2CS_SLAVE_PENDING | AST2600_I2CS_TX_NAK | AST2600_I2CS_STOP |
+		AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_RX_DONE:
+	case AST2600_I2CS_SLAVE_PENDING | AST2600_I2CS_TX_NAK | AST2600_I2CS_STOP |
+		AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_RX_DONE | AST2600_I2CS_WAIT_RX_DMA:
 	case AST2600_I2CS_TX_NAK | AST2600_I2CS_STOP:
 	case AST2600_I2CS_STOP:
 		cmd = SLAVE_TRIGGER_CMD;
