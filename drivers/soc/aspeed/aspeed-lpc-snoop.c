7f16b61d92414bc2a5c95f8ffd30175872ed32bf
Kevin Chen <kevin_chen@aspeedtech.com>
2025-07-22 14:20:14 +0800

    soc/aspeed: Mask and update HICR6 register in IRQ handler
    Masks the HICR6 register bits using a defined mask(bit0~16 is status
    bits) before updating it, ensuring only relevant bits are modified during
    the IRQ acknowledgment.
    This improves correctness and prevents unintended changes to other bits.
    
    Change-Id: Ie5074e852d531dd58622aebe0aa047c6aa71b122
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- drivers/soc/aspeed/aspeed-lpc-snoop.c
+++ drivers/soc/aspeed/aspeed-lpc-snoop.c
@@ -34,6 +34,7 @@
 #define HICR5_EN_SNP1W		BIT(2)
 #define HICR5_ENINT_SNP1W	BIT(3)
 #define HICR6	0x84
+#define HICR6_STR_MASK		GENMASK(16, 0)
 #define HICR6_STR_SNP0W		BIT(0)
 #define HICR6_STR_SNP1W		BIT(1)
 #define SNPWADR	0x90
@@ -173,7 +174,8 @@ static irqreturn_t aspeed_lpc_snoop_irq(int irq, void *arg)
 		return IRQ_NONE;
 
 	/* Ack pending IRQs */
-	regmap_write(lpc_snoop->regmap, HICR6, reg);
+	reg &= HICR6_STR_MASK;
+	regmap_update_bits(lpc_snoop->regmap, HICR6, reg, reg);
 
 	/* Read and save most recent snoop'ed data byte to FIFO */
 	regmap_read(lpc_snoop->regmap, SNPWDR, &data);
