96b92090dc6686b85df8d1407800d7b18ebfe4a7
Kevin Chen <kevin_chen@aspeedtech.com>
2025-07-24 11:41:11 +0800

    soc/aspeed: lpc_snoop: Handle NFE workaround in LPC snoop IRQ
    Add support for handling the Non-Fatal Error (NFE) workaround in the
    LPC snoop interrupt routine. Check for the presence of the NFE
    workaround flag and ensure it is preserved when acknowledging
    interrupts. This prevents unintended clearing of the NFE flag during
    IRQ handling.
    
    Improves robustness in scenarios where the NFE workaround is required.
    
    Change-Id: Ib5ae72384efc18588458abe74f370b0d43fcab82
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- drivers/soc/aspeed/aspeed-lpc-snoop.c
+++ drivers/soc/aspeed/aspeed-lpc-snoop.c
@@ -34,9 +34,9 @@
 #define HICR5_EN_SNP1W		BIT(2)
 #define HICR5_ENINT_SNP1W	BIT(3)
 #define HICR6	0x84
-#define HICR6_STR_MASK		GENMASK(16, 0)
 #define HICR6_STR_SNP0W		BIT(0)
 #define HICR6_STR_SNP1W		BIT(1)
+#define HICR6_NFE_WA		BIT(20)
 #define SNPWADR	0x90
 #define SNPWADR_CH0_MASK	GENMASK(15, 0)
 #define SNPWADR_CH0_SHIFT	0
@@ -169,13 +169,19 @@ static irqreturn_t aspeed_lpc_snoop_irq(int irq, void *arg)
 		return IRQ_NONE;
 
 	/* Check if one of the snoop channels is interrupting */
-	reg &= (HICR6_STR_SNP0W | HICR6_STR_SNP1W);
-	if (!reg)
+	if (!(reg & (HICR6_STR_SNP0W | HICR6_STR_SNP1W)))
 		return IRQ_NONE;
 
-	/* Ack pending IRQs */
-	reg |= HICR6_STR_MASK;
-	regmap_update_bits(lpc_snoop->regmap, HICR6, reg, reg);
+	/* Check if NFE WA is set */
+	if (reg & HICR6_NFE_WA) {
+		/* Ack pending IRQs with keeping NFE WA */
+		regmap_write(lpc_snoop->regmap, HICR6,
+			     (HICR6_STR_SNP0W | HICR6_STR_SNP1W | HICR6_NFE_WA));
+	} else {
+		/* Ack pending IRQs */
+		regmap_write(lpc_snoop->regmap, HICR6,
+			     (HICR6_STR_SNP0W | HICR6_STR_SNP1W));
+	}
 
 	/* Read and save most recent snoop'ed data byte to FIFO */
 	regmap_read(lpc_snoop->regmap, SNPWDR, &data);
