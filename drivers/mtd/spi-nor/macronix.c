65e232a410416a574614a19457cd26c9f32a5f6d
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2025-08-10 12:54:11 +0800

    mtd: spi: macronix: Support Quad SPI page program
    In Macronix 4BAI table, Quad SPI is not supported.
    But, actually, 32h or 34h is supported. This patch
    adds a fixup callback function to mitigate this problem.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I30f40f071b537c02a7164118dc2b05e9f2e647fc
    (cherry picked from commit b5ef59d4cffe6299941ce018ce6423c9b5cc72b5)

--- drivers/mtd/spi-nor/macronix.c
+++ drivers/mtd/spi-nor/macronix.c
@@ -28,8 +28,28 @@ mx25l25635_post_bfpt_fixups(struct spi_nor *nor,
 	return 0;
 }
 
+static int macronix_post_sfdp_fixups(struct spi_nor *nor)
+{
+	struct spi_nor_flash_parameter *params = nor->params;
+
+	params->hwcaps.mask |= SNOR_HWCAPS_PP_1_1_4;
+	if (!!(nor->flags & (SNOR_F_4B_OPCODES | SNOR_F_HAS_4BAIT))) {
+		spi_nor_set_pp_settings(&params->page_programs[SNOR_CMD_PP_1_1_4],
+					SPINOR_OP_PP_1_1_4_4B, SNOR_PROTO_1_1_4);
+	} else {
+		spi_nor_set_pp_settings(&params->page_programs[SNOR_CMD_PP_1_1_4],
+					SPINOR_OP_PP_1_1_4, SNOR_PROTO_1_1_4);
+	}
+
+	return 0;
+}
+
 static const struct spi_nor_fixups mx25l25635_fixups = {
 	.post_bfpt = mx25l25635_post_bfpt_fixups,
+};
+
+static const struct spi_nor_fixups macronix_qspi_pp_fixups = {
+	.post_sfdp = macronix_post_sfdp_fixups,
 };
 
 static const struct flash_info macronix_nor_parts[] = {
