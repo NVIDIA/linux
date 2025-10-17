92f2836435c8b0a36579cf8799364b828d8d4026
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2025-10-17 21:38:01 +0800

    spi-nor: Support daul/quad SPI mode for w25q64jvm
    Add dual/quad SPI mode for w25q64jvm.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I421d836fcfe64c714fd427f642548857ca8502a1

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -350,7 +350,7 @@ static const struct flash_info winbond_nor_parts[] = {
 		.id = SNOR_ID(0xef, 0x70, 0x17),
 		.name = "w25q64jvm",
 		.size = SZ_8M,
-		.no_sfdp_flags = SECT_4K,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
 	}, {
 		.id = SNOR_ID(0xef, 0x70, 0x18),
 		.name = "w25q128jv",
