1f2b4968bf761ed202cf1b6180a05aed0df366f6
Jammy Huang <jammy_huang@aspeedtech.com>
2025-10-18 09:12:02 +0800

    soc/aspeed: lpc_snoop: Support multi-instance
    snoop supports up to 2 channels per instance. It used hw-index as
    devname, and it leads to name conflict if there is multi-instances.
    
    Use dynamic-id for devname to fix the issue.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Change-Id: I8e5c3d826652249b0ed98b07780b9b9edced8c0d

--- drivers/soc/aspeed/aspeed-lpc-snoop.c
+++ drivers/soc/aspeed/aspeed-lpc-snoop.c
@@ -101,6 +101,8 @@ static const struct aspeed_lpc_snoop_channel_cfg channel_cfgs[ASPEED_LPC_SNOOP_I
 	},
 };
 
+static DEFINE_IDA(aspeed_lpc_snoop_ida);
+
 static struct aspeed_lpc_snoop_channel *snoop_file_to_chan(struct file *file)
 {
 	return container_of(file->private_data,
@@ -221,7 +223,7 @@ static int aspeed_lpc_enable_snoop(struct device *dev,
 				    u16 lpc_port)
 {
 	const struct aspeed_lpc_snoop_model_data *model_data;
-	int rc = 0;
+	int rc = 0, id;
 
 	if (WARN_ON(channel->enabled))
 		return -EBUSY;
@@ -233,8 +235,12 @@ static int aspeed_lpc_enable_snoop(struct device *dev,
 	channel->miscdev.fops = &snoop_fops;
 	channel->miscdev.parent = dev;
 
+	id = ida_alloc(&aspeed_lpc_snoop_ida, GFP_KERNEL);
+	if (id < 0)
+		return id;
+
 	channel->miscdev.name =
-		devm_kasprintf(dev, GFP_KERNEL, "%s%d", DEVICE_NAME, cfg->index);
+		devm_kasprintf(dev, GFP_KERNEL, "%s%d", DEVICE_NAME, id);
 	if (!channel->miscdev.name)
 		return -ENOMEM;
 
