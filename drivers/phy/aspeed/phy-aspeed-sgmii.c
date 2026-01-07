bb2c72de1a7925c4c4b452774cabc0e5f4f44818
Jacky Chou <jacky_chou@aspeedtech.com>
2026-01-07 13:56:49 +0800

    phy: aspeed: refactor sgmii driver
    Split nway and fixed-link
    
    Signed-off-by: Jacky Chou <jacky_chou@aspeedtech.com>
    Change-Id: I7803114c43191d42ca87ee6b510e329207074568

--- drivers/phy/aspeed/phy-aspeed-sgmii.c
+++ drivers/phy/aspeed/phy-aspeed-sgmii.c
@@ -11,76 +11,94 @@
 #include <linux/ethtool.h>
 
 #define SGMII_CFG			0x00
+#define   SGMII_CFG_FIFO_MODE			BIT(0)
+#define   SGMII_CFG_SPEED_SEL_MASK		GENMASK(5, 4)
+#define   SGMII_CFG_SPEED_SEL(x)		FIELD_PREP(SGMII_CFG_SPEED_SEL_MASK, (x))
+#define   SGMII_CFG_PWR_DOWN			BIT(11)
+#define   SGMII_CFG_AN_ENABLE			BIT(12)
+#define  SGMII_CFG_SW_RESET			BIT(15)
 #define SGMII_LINK_TIMER		0x08
 #define SGMII_NWAY_ACK			0x0c
 #define SGMII_PHY_CFG1			0x18
+#define   SGMII_PHY_SPEED_MASK			GENMASK(3, 2)
+#define   SGMII_PHY_SPEED(x)			FIELD_PREP(SGMII_PHY_SPEED_MASK, (x))
 #define SGMII_PHY_PIPE_CTL		0x20
+#define   SGMII_PCTL_TX_NO_DEEMPH		BIT(7)
 #define SGMII_FIFO_DELAY_THREHOLD	0x28
 #define SGMII_MODE			0x30
+#define   SGMII_MODE_ENABLE			BIT(0)
+#define   SGMII_MODE_USE_LOCAL_CONFIG		BIT(2)
 
-#define SGMII_CFG_FIFO_MODE		BIT(0)
-#define SGMII_CFG_SPEED_10M		0
-#define SGMII_CFG_SPEED_100M		BIT(4)
-#define SGMII_CFG_SPEED_1G		BIT(5)
-#define SGMII_CFG_PWR_DOWN		BIT(11)
-#define SGMII_CFG_AN_ENABLE		BIT(12)
-#define SGMII_CFG_SW_RESET		BIT(15)
-#define SGMII_PCTL_TX_NO_DEEMPH		BIT(7)
-#define SGMII_MODE_ENABLE		BIT(0)
-#define SGMII_MODE_USE_LOCAL_CONFIG	BIT(2)
+#define PCIEPHY_CLK			0x268
+#define   PCIEPHY_CLK_FREQ_MULTI_MASK		GENMASK(7, 0)
+#define   PCIEPHY_CLK_FREQ_MULTI(x)		FIELD_PREP(PCIEPHY_CLK_FREQ_MULTI_MASK, (x))
+#define   PCIEPHY_CLK_SEL_INTERNAL_25M		BIT(8)
 
-#define PLDA_CLK		0x268
-
-#define PLDA_CLK_SEL_INTERNAL_25M	BIT(8)
-#define PLDA_CLK_FREQ_MULTI		GENMASK(7, 0)
+#define SGMII_SPEED_10M		0x00
+#define SGMII_SPEED_100M	0x01
+#define SGMII_SPEED_1G		0x02
 
 struct aspeed_sgmii {
 	struct device *dev;
 	void __iomem *regs;
-	struct regmap *plda_regmap;
+	struct regmap *pcie_phy_regmap;
 };
 
-static void aspeed_sgmii_set_nway(struct phy *phy)
+static int aspeed_sgmii_conf(struct phy *phy, bool nway, int speed)
 {
 	struct aspeed_sgmii *sgmii = phy_get_drvdata(phy);
-	u32 reg;
-
-	/*
-	 * The PLDA frequency multiplication is X xor 0x19.
-	 * (X xor 0x19) * clock source = data rate.
-	 * SGMII data rate is 1.25G, so (0x2b xor 0x19) * 25MHz is equal 1.25G.
-	 */
-	reg = PLDA_CLK_SEL_INTERNAL_25M | FIELD_PREP(PLDA_CLK_FREQ_MULTI, 0x2b);
-	regmap_write(sgmii->plda_regmap, PLDA_CLK, reg);
+	u32 cfg;
 
 	writel(0, sgmii->regs + SGMII_MODE);
 
 	writel(0, sgmii->regs + SGMII_CFG);
-	reg = SGMII_CFG_SW_RESET | SGMII_CFG_PWR_DOWN;
-	writel(reg, sgmii->regs + SGMII_CFG);
-
-	reg = SGMII_CFG_AN_ENABLE;
-	writel(reg, sgmii->regs + SGMII_CFG);
+	writel(SGMII_CFG_SW_RESET | SGMII_CFG_PWR_DOWN, sgmii->regs + SGMII_CFG);
+	if (nway) {
+		writel(SGMII_CFG_AN_ENABLE, sgmii->regs + SGMII_CFG);
+	} else {
+		switch (speed) {
+		case SPEED_10:
+			cfg = SGMII_SPEED_10M;
+			break;
+		case SPEED_100:
+			cfg = SGMII_SPEED_100M;
+			break;
+		case SPEED_1000:
+			cfg = SGMII_SPEED_1G;
+			break;
+		default:
+			return -EINVAL;
+		}
+		writel(SGMII_PHY_SPEED(cfg), sgmii->regs + SGMII_PHY_CFG1);
+		writel(SGMII_CFG_SPEED_SEL(cfg), sgmii->regs + SGMII_CFG);
+	}
 
 	writel(0x0c, sgmii->regs + SGMII_FIFO_DELAY_THREHOLD);
-
 	writel(SGMII_PCTL_TX_NO_DEEMPH, sgmii->regs + SGMII_PHY_PIPE_CTL);
 
-	/* Set link timer for Nway state change */
+	/* Set link timer for state change */
 	writel(0x100, sgmii->regs + SGMII_LINK_TIMER);
 
 	/* Bit 0 always sets to 1 in ACK message */
 	writel(0x1, sgmii->regs + SGMII_NWAY_ACK);
 
-	reg = SGMII_MODE_ENABLE;
-	writel(reg, sgmii->regs + SGMII_MODE);
+	cfg = SGMII_MODE_ENABLE;
+	if (!nway)
+		cfg |= SGMII_MODE_USE_LOCAL_CONFIG;
+	writel(cfg, sgmii->regs + SGMII_MODE);
+
+	return 0;
 }
 
 static int aspeed_sgmii_phy_init(struct phy *phy)
 {
-	aspeed_sgmii_set_nway(phy);
+	/* Default to enable Nway, not need configure speed */
+	return aspeed_sgmii_conf(phy, true, 0);
+}
 
-	return 0;
+static int aspeed_sgmii_phy_set_speed(struct phy *phy, int speed)
+{
+	return aspeed_sgmii_conf(phy, false, speed);
 }
 
 static int aspeed_sgmii_phy_exit(struct phy *phy)
@@ -89,50 +107,6 @@ static int aspeed_sgmii_phy_exit(struct phy *phy)
 
 	/* Disable SGMII controller */
 	writel(0, sgmii->regs + SGMII_MODE);
-
-	return 0;
-}
-
-static int aspeed_sgmii_phy_set_speed(struct phy *phy, int speed)
-{
-	struct aspeed_sgmii *sgmii = phy_get_drvdata(phy);
-	u32 reg;
-
-	reg = PLDA_CLK_SEL_INTERNAL_25M | FIELD_PREP(PLDA_CLK_FREQ_MULTI, 0x2b);
-	regmap_write(sgmii->plda_regmap, PLDA_CLK, reg);
-
-	switch (speed) {
-	case SPEED_10:
-		reg = SGMII_CFG_SPEED_10M;
-		break;
-	case SPEED_100:
-		reg = SGMII_CFG_SPEED_100M;
-		break;
-	case SPEED_1000:
-		reg = SGMII_CFG_SPEED_1G;
-		break;
-	default:
-		return -EINVAL;
-	}
-
-	writel(0, sgmii->regs + SGMII_MODE);
-
-	writel((reg >> 2), sgmii->regs + SGMII_PHY_CFG1);
-
-	writel(0, sgmii->regs + SGMII_CFG);
-	writel(SGMII_CFG_SW_RESET | SGMII_CFG_PWR_DOWN, sgmii->regs + SGMII_CFG);
-	writel(reg, sgmii->regs + SGMII_CFG);
-
-	writel(0x0c, sgmii->regs + SGMII_FIFO_DELAY_THREHOLD);
-	writel(SGMII_PCTL_TX_NO_DEEMPH, sgmii->regs + SGMII_PHY_PIPE_CTL);
-
-	/* Set link timer for Nway state change */
-	writel(0x100, sgmii->regs + SGMII_LINK_TIMER);
-
-	/* Bit 0 always sets to 1 in ACK message */
-	writel(0x1, sgmii->regs + SGMII_NWAY_ACK);
-
-	writel(SGMII_MODE_ENABLE | SGMII_MODE_USE_LOCAL_CONFIG, sgmii->regs + SGMII_MODE);
 
 	return 0;
 }
@@ -146,12 +120,13 @@ static const struct phy_ops aspeed_sgmii_phyops = {
 
 static int aspeed_sgmii_probe(struct platform_device *pdev)
 {
+	struct phy_provider *provider;
 	struct aspeed_sgmii *sgmii;
+	struct device_node *np;
 	struct resource *res;
 	struct device *dev;
-	struct device_node *np;
-	struct phy_provider *provider;
 	struct phy *phy;
+	u32 reg;
 
 	dev = &pdev->dev;
 
@@ -174,11 +149,11 @@ static int aspeed_sgmii_probe(struct platform_device *pdev)
 	}
 
 	np = pdev->dev.of_node;
-	sgmii->plda_regmap = syscon_regmap_lookup_by_phandle(np, "aspeed,plda");
-	if (IS_ERR(sgmii->plda_regmap)) {
-		dev_err(sgmii->dev, "Unable to find plda regmap (%ld)\n",
-			PTR_ERR(sgmii->plda_regmap));
-		return PTR_ERR(sgmii->plda_regmap);
+	sgmii->pcie_phy_regmap = syscon_regmap_lookup_by_phandle(np, "phys");
+	if (IS_ERR(sgmii->pcie_phy_regmap)) {
+		dev_err(sgmii->dev, "Unable to find phys regmap (%ld)\n",
+			PTR_ERR(sgmii->pcie_phy_regmap));
+		return PTR_ERR(sgmii->pcie_phy_regmap);
 	}
 
 	phy = devm_phy_create(dev, NULL, &aspeed_sgmii_phyops);
@@ -192,6 +167,14 @@ static int aspeed_sgmii_probe(struct platform_device *pdev)
 		return PTR_ERR(provider);
 
 	phy_set_drvdata(phy, sgmii);
+
+	/*
+	 * The PLDA frequency multiplication is X xor 0x19.
+	 * (X xor 0x19) * clock source = data rate.
+	 * SGMII data rate is 1.25G, so (0x2b xor 0x19) * 25MHz is equal 1.25G.
+	 */
+	reg = PCIEPHY_CLK_SEL_INTERNAL_25M | PCIEPHY_CLK_FREQ_MULTI(0x2b);
+	regmap_write(sgmii->pcie_phy_regmap, PCIEPHY_CLK, reg);
 
 	dev_info(dev, "module loaded\n");
 
@@ -214,5 +197,5 @@ static struct platform_driver aspeed_sgmii_driver = {
 module_platform_driver(aspeed_sgmii_driver);
 
 MODULE_AUTHOR("Jacky Chou <jacky_chou@aspeedtech.com>");
-MODULE_DESCRIPTION("Control of ASPEED SGMII Device");
+MODULE_DESCRIPTION("ASPEED SGMII Serdes/PHY controller");
 MODULE_LICENSE("GPL");
