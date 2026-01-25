1bd587461cf19901d9b26079e94eb07b3d76fce9
Chester Lin <chesterl@nvidia.com>
2026-01-25 05:27:16 -0800

    i2c-ast2600: a DT property to adjust tout base-clk for bad response time tolerance
    On some devices/applications we noticed that sometimes their SMBus
    transactions could violate T-timeout criteria (<=25ms), which evneutally
    breaks data messaging. For non-SMBus I2C traffic, the current maximum time
    (32ms) could be still not enough for some corner cases.
    
    Here we introduce a new DT property "tout-baseclk-div", which allows
    timeout base-clk adjustment so i2c controllers can configure a longer
    expiration time. (Suggested by ASPEED)
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5168121
    
    Signed-off-by: Chester Lin <chesterl@nvidia.com>

--- drivers/i2c/busses/i2c-ast2600.c
+++ drivers/i2c/busses/i2c-ast2600.c
@@ -293,6 +293,20 @@
 
 #define AST2600_I2C_TIMEOUT_CLK		0x1
 #define AST2700_I2C_TIMEOUT_CLK		0x3
+/*
+ * Timeout base clk divisor, ICC04[9:8]
+ * tout_baseclk_0 (0x0 = 00'b): base-clk4 /  256
+ * tout_baseclk_1 (0x1 = 01'b): base-clk4 / 1024
+ * tout_baseclk_2 (0x2 = 10'b): base-clk4 / 4096
+ * tout_baseclk_3 (0x3 = 11'b): base-clk4 / 8192
+ */
+#define AST_I2C_TIMEOUT_CLK_DIV256	0x0
+#define AST_I2C_TIMEOUT_CLK_DIV1024	0x1
+#define AST_I2C_TIMEOUT_CLK_DIV4096	0x2
+#define AST_I2C_TIMEOUT_CLK_DIV8192	0x3
+
+#define AST2600_I2C_TIMEOUT_CLK		AST_I2C_TIMEOUT_CLK_DIV1024
+#define AST2700_I2C_TIMEOUT_CLK		AST_I2C_TIMEOUT_CLK_DIV8192
 
 #define AST2600_I2C_TARGET_COUNT		0x3
 
@@ -361,7 +375,50 @@ struct ast2600_i2c_bus {
 	 * ICC04[23:20]: tCkHighMin
 	 */
 	u32 tck_thddat;
+	u32 tout_baseclk_div;
 };
+
+static u32 ast2600_fix_tout_baseclk_div(struct ast2600_i2c_bus *i2c_bus)
+{
+	u32 div_val;
+
+	switch (i2c_bus->tout_baseclk_div) {
+	case 256:
+		div_val = AST_I2C_TIMEOUT_CLK_DIV256;
+		break;
+	case 1024:
+		div_val = AST_I2C_TIMEOUT_CLK_DIV1024;
+		break;
+	case 4096:
+		div_val = AST_I2C_TIMEOUT_CLK_DIV4096;
+		break;
+	case 8192:
+		div_val = AST_I2C_TIMEOUT_CLK_DIV8192;
+		break;
+	default:
+		dev_dbg(i2c_bus->dev,
+			"Unexpected tout-baseclk-div value: %u. Fix to default (1024)",
+			i2c_bus->tout_baseclk_div);
+
+		div_val = AST_I2C_TIMEOUT_CLK_DIV1024;
+		i2c_bus->tout_baseclk_div = 1024;
+	}
+
+	dev_info(i2c_bus->dev, "tout_baseclk_div: %u", div_val);
+
+	return div_val;
+}
+
+static u32 ast2600_calc_timeout_timer(struct ast2600_i2c_bus *i2c_bus)
+{
+	u32 timeout_val;
+
+	timeout_val = i2c_bus->timeout / i2c_bus->tout_baseclk_div;
+
+	dev_dbg(i2c_bus->dev, "timeout ticks: %#x", timeout_val);
+
+	return timeout_val;
+}
 
 static void ast2600_i2c_ac_timing_config(struct ast2600_i2c_bus *i2c_bus)
 {
@@ -401,9 +458,14 @@ static void ast2600_i2c_ac_timing_config(struct ast2600_i2c_bus *i2c_bus)
 	}
 
 	if (i2c_bus->timeout) {
-		i2c_bus->timeout = min(i2c_bus->timeout, 31);
-		data |= AST2600_I2CC_TTIMEOUT(i2c_bus->timeout);
-		data |= AST2600_I2CC_TOUTBASECLK(AST2600_I2C_TIMEOUT_CLK);
+		u32 tout_clkdiv = ast2600_fix_tout_baseclk_div(i2c_bus);
+		u32 tout_ticks = ast2600_calc_timeout_timer(i2c_bus);
+
+		/* ast2600 only have [4:0] range */
+		if (tout_ticks > 31)
+			tout_ticks = 31;
+		data |= AST2600_I2CC_TTIMEOUT(tout_ticks);
+		data |= AST2600_I2CC_TOUTBASECLK(tout_clkdiv);
 	}
 
 	writel(data, i2c_bus->reg_base + AST2600_I2CC_AC_TIMING);
@@ -2355,24 +2417,16 @@ static int ast2600_i2c_probe(struct platform_device *pdev)
 
 	/*
 	 * i2c timeout counter: use base clk4 1Mhz,
-	 * per unit: 1/(1000/1024) = 1024us
-	 * lower than 1024 us will be set 1 ms.
+	 * per unit: 1/(1000/tout-baseclk-div)
+	 * Example of divisor 1024: 1/(1000/1024) = 1024us
 	 */
 	ret = device_property_read_u32(dev, "i2c-scl-clk-low-timeout-us", &i2c_bus->timeout);
-	if (!ret) {
-		i2c_bus->timeout /= 1024;
 
-		if (!i2c_bus->timeout)
-			i2c_bus->timeout = 1;
-	}
-
-	/*
-	 * i2c timeout counter: use base clk4 1Mhz,
-	 * per unit: 1/(1000/1024) = 1024us
-	 */
 	ret = device_property_read_u32(dev, "i2c-tck-thddat-config", &i2c_bus->tck_thddat);
 	if (!ret)
 		dev_info(&pdev->dev, "Manual tCLK* & tHDDAT settings: %#08x", i2c_bus->tck_thddat);
+
+	ret = device_property_read_u32(dev, "tout-baseclk-div", &i2c_bus->tout_baseclk_div);
 
 	init_completion(&i2c_bus->cmd_complete);
 
