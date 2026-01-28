e5daf18c49bea0308f08c339a98b00fadd7dd339
Jammy Huang <jammy_huang@aspeedtech.com>
2026-01-28 14:13:19 +0800

    media: aspeed: Fix capture from MEM NG
    s_input is how MEM trigger operation.  Upstream's modification will
    block it.
    
    Remove condition check.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Change-Id: I44b879866d4076cff51f909f23df587c80fdb152

--- drivers/media/platform/aspeed/aspeed-video.c
+++ drivers/media/platform/aspeed/aspeed-video.c
@@ -1948,12 +1948,6 @@ static int aspeed_video_set_input(struct file *file, void *fh, unsigned int i)
 	if (i >= VIDEO_INPUT_MAX)
 		return -EINVAL;
 
-	if (i == video->input)
-		return 0;
-
-	if (vb2_is_busy(&video->queue))
-		return -EBUSY;
-
 	if (IS_ERR(video->scu)) {
 		v4l2_dbg(1, debug, &video->v4l2_dev,
 			 "%s: scu isn't ready for input-control\n", __func__);
