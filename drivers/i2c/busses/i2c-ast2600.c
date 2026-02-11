611f3007ad599a78e48e7dc65524d1eccc4836eb
Tommy Huang <tommy_huang@aspeedtech.com>
2026-02-11 10:06:06 +0800

    i2c: ast2600: update reset controller behavior
    Update reset controller behavior.
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: I78caf8b9dfd9fee18bb9aa95d348123987cbf173

--- drivers/i2c/busses/i2c-ast2600.c
+++ drivers/i2c/busses/i2c-ast2600.c
@@ -551,17 +551,15 @@ static void ast2700_i2c_ac_timing_config(struct ast2600_i2c_bus *i2c_bus)
 static int ast2600_i2c_recover_bus(struct ast2600_i2c_bus *i2c_bus)
 {
 	u32 state = readl(i2c_bus->reg_base + AST2600_I2CC_STS_AND_BUFF);
-	u32 ctrl = readl(i2c_bus->reg_base + AST2600_I2CC_FUN_CTRL);
+	u32 ctrl;
 	int ret = 0;
 	int r;
 
 	dev_dbg(i2c_bus->dev, "%d-bus recovery bus [%x]\n", i2c_bus->adap.nr, state);
 
 	/* reset i2c controller to avoid the bus getting stuck */
-	ctrl &= ~(AST2600_I2CC_MASTER_EN);
-	writel(ctrl, i2c_bus->reg_base + AST2600_I2CC_FUN_CTRL);
-
-	ctrl |= (AST2600_I2CC_MASTER_EN);
+	ctrl = readl(i2c_bus->reg_base + AST2600_I2CC_FUN_CTRL);
+	writel(ctrl & ~AST2600_I2CC_MASTER_EN, i2c_bus->reg_base + AST2600_I2CC_FUN_CTRL);
 	writel(ctrl, i2c_bus->reg_base + AST2600_I2CC_FUN_CTRL);
 
 	reinit_completion(&i2c_bus->cmd_complete);
