a9c1f56a5938ac2b7fb70723a48406895fc44c6a
Alex Hoganson <ahoganson@nvidia.com>
2026-04-28 14:26:01 -0700

    aspeed-espi: Include guard header, function proto vars
    Add include guards to the aspeed-espi header to enable multiple includes
    without conflict. Defines function protos to have descriptive params.
    
    Signed-off-by: Alex Hoganson <ahoganson@nvidia.com>

--- include/linux/soc/aspeed/aspeed-espi.h
+++ include/linux/soc/aspeed/aspeed-espi.h
@@ -1,6 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /* Copyright (c) 2023 Intel Corporation. */
 
-typedef void (*aspeed_espi_irq_handler)(int, void *);
-void aspeed_espi_register_gpio(struct device *espi_ctrl,
+#ifndef _LINUX_SOC_ASPEED_ESPI_H
+#define _LINUX_SOC_ASPEED_ESPI_H
+
+typedef void (*aspeed_espi_irq_handler)(int irq, void *data);
+void aspeed_espi_register_gpio(struct device *dev,
 			       aspeed_espi_irq_handler handler, void *data);
+
+#endif /* _LINUX_SOC_ASPEED_ESPI_H */
